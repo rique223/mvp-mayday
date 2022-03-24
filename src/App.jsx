7a555d8e61d5b94aef763141fcf042afb7c34581
Matheus Braz <matheus.braz14@gmail.com>
2022-03-24 00:09:31 -0300

    tela de contingencia em andamento

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CidadesContextProvider } from "./Context/CidadesContext";
 import HomeScreen from "./Pages/HomeScreen";
+import ContingenciaScreen from "./Pages/HomeScreen/ContingenciaScreen";
 import ModulosScreen from "./Pages/HomeScreen/ModulosScreen";
 
 const App = () => {
@@ -11,10 +12,11 @@ const App = () => {
         <Routes>
           <Route path="/" element={<HomeScreen />} />
           <Route path="modulos" element={<ModulosScreen />} />
+          <Route path="contingencia/:idCidade" element={<ContingenciaScreen />} />
         </Routes>
       </BrowserRouter>
     </CidadesContextProvider>
   );
 };
-
+ 
 export default App;
